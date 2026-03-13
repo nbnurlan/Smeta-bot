aef353b653a2339082d8bc8c9d40811f50a6c952
nbnurlan <125850954+nbnurlan@users.noreply.github.com>
2026-03-13 21:40:05 +0500

    Update MASTER_PASSWORD default value in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN: str = os.getenv("8604814040:AAHq4lSnkwaOEoAeXh7rw-QBQIl51IZqrFI", "")
-MASTER_PASSWORD: str = os.getenv("MASTER_PASSWORD", "usta2026")
+MASTER_PASSWORD: str = os.getenv("MASTER_PASSWORD", "2026")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN .env faylida belgilanmagan!")
