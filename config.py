2be6abd4e384a1442fff002740b6faf5097fb8f1
nbnurlan <125850954+nbnurlan@users.noreply.github.com>
2026-03-13 17:19:06 +0500

    Update BOT_TOKEN and MASTER_PASSWORD values

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN: str = os.getenv("8604814040:AAHq4lSnkwaOEoAeXh7rw-QBQIl51IZqrFI", "")
+MASTER_PASSWORD: str = os.getenv("MASTER_PASSWORD", "usta2026")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN .env faylida belgilanmagan!")
